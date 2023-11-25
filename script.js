10290e643cdbd8fc51b7eb57ca049a3cd41da8df
Omolade Lekan <omoladelekan99@gmail.com>
2023-11-25 18:32:34 +0100

    worked on the hamburger menu

--- script.js
+++ script.js
@@ -3,16 +3,22 @@
 
 const navBar = document.querySelector("#navBar").children;
 const menuBar = document.querySelector(".menuBar");
+const strokes = Array.from(document.querySelectorAll(".menuBar div"));
 
 const iconsNavlink = Array.from(navBar).slice(1);
 
-
+console.log(strokes);
 
 
 menuBar.addEventListener('click', () => {
     iconsNavlink.forEach(child => {
         child.classList.toggle("hide");
-    })
+    });
+
+    strokes[0].classList.toggle("one");
+    strokes[1].classList.toggle("two");
+    strokes[2].classList.toggle("three");
+
 });
 
 
