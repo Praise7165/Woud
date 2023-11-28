1c458ab159b819863849ae8acc4039694f405c94
Omolade Lekan <omoladelekan99@gmail.com>
2023-11-28 05:49:47 +0100

    worked on the category section

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const header = document.querySelector("header");
 const menuBar = document.querySelector(".menuBar");
 const strokes = Array.from(document.querySelectorAll(".menuBar div"));
+const rooms = Array.from(document.querySelectorAll(".room"));
 
 
 
@@ -18,8 +19,37 @@ menuBar.addEventListener('click', () => {
 });
 
 
+rooms.forEach(room => {
+    
+
+    room.addEventListener("mouseover", () => {
+
+        let roomName = room.querySelector("div > span");
+        let arrow = room.querySelector("div > img");
+        let image = room.querySelector(".room-image");
 
 
+        roomName.style.color = "#121212";
+        arrow.style.transform = "rotate(45deg)";
+        image.style.zIndex = "1000";
+
+        room.style.transition = "all .3s ease-in";
+
+
+
+        
+        room.addEventListener("mouseout", () => {
+            roomName.style.color = "#616161";
+            arrow.style.transform = "initial";
+            image.style.zIndex = "initial";
+
+            room.style.transition = "all .3s ease-in";
+        });
+        
+
+    });
+
+})
 
 
 
