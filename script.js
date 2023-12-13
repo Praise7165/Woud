2b51226dd5440c314b26058fbc165cbf2ba054f9
Omolade Lekan <omoladelekan99@gmail.com>
2023-12-13 07:20:07 +0100

    Worked on the slider

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-
-
 const header = document.querySelector("header");
 const menuBar = document.querySelector(".menuBar");
+const sliderWrapper = document.querySelector(".featured .container");
+const sliderContainer = document.querySelector(".product-wrapper");
+const controls = Array.from(document.querySelector(".featured .header-content > :last-child").children);
+const sliderItems = sliderContainer.querySelectorAll('.product-card');
 const strokes = Array.from(document.querySelectorAll(".menuBar div"));
 const rooms = Array.from(document.querySelectorAll(".room"));
 
-
+const leftArrow = controls[0];
+const rightArrow = controls[1];
 
 menuBar.addEventListener('click', () => {
 
@@ -49,7 +52,34 @@ rooms.forEach(room => {
 
     });
 
-})
+});
+
+
+
+const totalItemsWidth = sliderContainer.getBoundingClientRect().width;
+let currentIndex = 0;
+
+function moveSlider(direction) {
+  currentIndex += direction; // Update index based on click
+  if (currentIndex < 0) currentIndex = 0; // Prevent negative index
+  if (currentIndex >= sliderItems.length) currentIndex = sliderItems.length - 1; // Prevent exceeding item count
+
+  // Calculate new transform value
+  const newTransformX = -currentIndex * 320;
+
+  // Check for last item and adjust if needed
+  if (currentIndex === sliderItems.length - 1) {
+    newTransformX = -totalItemsWidth + sliderContainer.clientWidth;
+  }
+
+  // Update slider position
+  sliderContainer.style.transform = `translateX(${newTransformX}px)`;
+}
+
+// Bind click events to arrow buttons
+leftArrow.addEventListener("click", () => moveSlider(-1));
+rightArrow.addEventListener("click", () => moveSlider(1));
+
 
 
 
